0c5a26265dc7c11825c0c30dfd92365c29937b0d
DEV17 <89392749+DEV17CODER@users.noreply.github.com>
2023-07-01 19:28:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
+const form = document.querySelector("form");
+const statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e)=> {
     e.preventDefault();
     statusTxt.style.color = "#0D6EFD";
