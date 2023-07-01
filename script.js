466dcf94fd63d3d52bbd4423ae76a8dd3c3f292e
DEV17 <89392749+DEV17CODER@users.noreply.github.com>
2023-07-01 21:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var form = event.target;
-    var formData = new FormData(form);
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", form.action, true);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            document.getElementById("response").textContent = xhr.responseText;
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+form.onsubmit = (e)=> {
+    e.preventDefault();
+    statusTxt.style.color = "#0D6EFD";
+    statusTxt.style.display = "block";
+    statusTxt.innerText = "Sending your message...";
+    form.classList.add("disabled");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "message.php", true);
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let response = xhr.response;
+            if (response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1) {
+                statusTxt.style.color = "red";
+            } else {
+                form.reset();
+                setTimeout(() => {
+                    statusTxt.style.display = "none";
+                }, 3000);
+            }
+            statusTxt.innerText = response;
+            form.classList.remove("disabled");
         }
-    };
+    }
+    let formData = new FormData(form);
     xhr.send(formData);
-});
+}
