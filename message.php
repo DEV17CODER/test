4dfdc9c31ceb8e031f5aa4367dc3943457c79fca
DEV17 <89392749+DEV17CODER@users.noreply.github.com>
2023-07-01 18:21:58 +0530

    Update message.php

--- message.php
+++ message.php
@@ -1,25 +1,25 @@
 <?php
-$name = htmlspecialchars($_POST['name']);
-$email = htmlspecialchars($_POST['email']);
-$phone = htmlspecialchars($_POST['phone']);
-$website = htmlspecialchars($_POST['website']);
-$message = htmlspecialchars($_POST['message']);
-
-if (!empty($email) && !empty($message)) {
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $receiver = "17devrajparmar@gmail.com";
-        $subject = "From: $name <$email>";
-        $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards, \n$name";
-        $sender = "From: $email";
-        if (mail($receiver, $subject, $body, $sender)) {
-            echo "Your message has been sent";
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $website = htmlspecialchars($_POST['website']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    if (!empty($email) && !empty($message)) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $receiver = "17devrajparmar@gmail.com";
+            $subject = "From: $name <$email>";
+            $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards, \n$name";
+            $sender = "From: $email";
+            if (mail($receiver, $subject, $body, $sender)) {
+                echo "Your message has been sent";
+            } else {
+                echo "sorry, failed to send your message!";
+            }
         } else {
-            echo "sorry, failed to send your message!";
+            echo "Enter a valid email address!";
         }
     } else {
-        echo "Enter a valid email address!";
+        echo "Email and message field is required!";
     }
-} else {
-    echo "Email and message field is required!";
-}
 ?>
