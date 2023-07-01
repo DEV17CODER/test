06a267aa3346d0ce221f53a462dcc27b5be2d488
DEV17 <89392749+DEV17CODER@users.noreply.github.com>
2023-07-01 20:17:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,13 @@
-const form = document.querySelector("form");
-const statusTxt = form.querySelector(".button-area span");
-
-form.onsubmit = (e) => {
-    e.preventDefault();
-    statusTxt.style.color = "#0D6EFD";
-    statusTxt.style.display = "block";
-    statusTxt.innerText = "Sending your message...";
-    form.classList.add("disabled");
-
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "message.php", true);
-
-    xhr.onload = () => {
-        if (xhr.readyState == 4) {
-            if (xhr.status == 200) {
-                let response = JSON.parse(xhr.responseText);
-                if (response.status === "success") {
-                    form.reset();
-                    setTimeout(() => {
-                        statusTxt.style.display = "none";
-                    }, 3000);
-                } else {
-                    statusTxt.style.color = "red";
-                }
-                statusTxt.innerText = response.message;
-            } else {
-                statusTxt.style.color = "red";
-                statusTxt.innerText = "Failed to send your message!";
-            }
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var form = event.target;
+    var formData = new FormData(form);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", form.action, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.getElementById("response").textContent = xhr.responseText;
         }
-        form.classList.remove("disabled");
     };
-
-    xhr.onerror = () => {
-        statusTxt.style.color = "red";
-        statusTxt.innerText = "An error occurred. Please try again later.";
-        form.classList.remove("disabled");
-    };
-
-    let formData = new FormData(form);
     xhr.send(formData);
-};
+});
