790fdcc0d67ff6e8e56ccd806d83227589b98998
DEV17 <89392749+DEV17CODER@users.noreply.github.com>
2023-07-01 20:17:54 +0530

    Update and rename message.php to submit.php

---
+++ submit.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    // Here, you can perform any necessary processing or validation with the form data
+    // For this example, we'll just send a simple email
+    $to = "your-email@example.com";
+    $subject = "New Contact Form Submission";
+    $emailContent = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $emailContent, $headers)) {
+        echo "Thank you for your message! We'll get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
